2216e330d0bf24cf5726361dd0eadf7fc072a293
stephdan <stephdan@onid.oregonstate.edu>
2017-03-24 14:13:23 -0700

    Rest of previous backup

--- LTVis.js
+++ LTVis.js
@@ -147,6 +147,8 @@ LTVis.Map = (function() {
   };
 })();
 
+// TODO: Lots.
+// Dates are currently just years, yyyy. Could be updates to dd/mm/yyyy
 LTVis.TimelineChart = function(divID, initialData){
   "use strict";
 
@@ -569,6 +571,35 @@ $.extend(LTVis, {
   init: function() {
     LTVis.GUI.init();
     LTVis.Map.init();
+
+    var data = [
+      {date: 1970, value: 10},
+      {date: 1981, value: 20},
+      {date: 1982, value: 30},
+      {date: 1983, value: 10},
+      {date: 1984, value: -90},
+      {date: 1985, value: 80},
+      {date: 1986, value: 120},
+      {date: 1987, value: 2},
+      {date: 1988, value: 40},
+      {date: 1989, value: 50},
+      {date: 1991, value: 111},
+      {date: 1993, value: 22},
+      {date: 1994, value: 20},
+      {date: 1997, value: 33},
+      {date: 2000, value: 60},
+      {date: 2006, value: 99},
+      {date: 2011, value: 50},
+    ];
+
+    // Demo-ing the timeline here.
+    // TODO move this code into the GUI module when it's really time to add this.
+    var timechart = new LTVis.TimelineChart("timeRangeContainer", data);
+    timechart.on("change", function() {
+      console.log(timechart.getSelectedDataPoint());
+    });
+
+    LTVis.activeTimelineChart = timechart;
   },
 
   loadSummaryData: function(pathToGeoJSON, pathToData, config) {
@@ -627,6 +658,8 @@ $.extend(LTVis, {
     });
   },
 
+  // FIXME: This is all hardcoded stuff for demonstration purposes. Needs to be
+  // extended and refined. 
   getRGBAColorFromGrayscale: function(n) {
     var c;
     if (LTVis.ramp === "BrBG") {
@@ -643,6 +676,7 @@ $.extend(LTVis, {
     return [c.r, c.g, c.b, c.opacity];
   },
 
+  // FIXME: This needs to be reviewed and updated to offer more control.
   CanvasLayer: L.GridLayer.extend({
     createTile: function(coords, done){
       var error;
@@ -691,6 +725,8 @@ $.extend(LTVis, {
     }
   }),
 
+  // TODO: This is all hardcoded stuff put here for demonstration purposes.
+  // Needs to be incorporated into a system that allows more control. 
   addCanvasLayer: function(layerID) {
     LTVis.ramp = "BrBG";
     LTVis.activeDataLayer = layerID;
@@ -702,26 +738,31 @@ $.extend(LTVis, {
     LTVis.Map.removeCanvasLayer();
   },
 
-  importTestCSV: function() {
+  loadDataset: function(datasetID) {
+    // Load the correct tiles, which are determined by the id and date of the 
+    // slider as of before this was called. 
 
-    var url = "data/premadeAreaSummaries/meanBioHexes/meanBioHexes_data.csv";
+    // TODO: Right now this just loads an existing tileset sitting in a folder.
+    // Later, this will need to make a request to Islay for specific tiles.
+    // ALSO, tiles cannot be styled unless they come from the same server 
+    // as this website. So we may need to have pre-styled tiles, in which case
+    // we don't need to addCanvasLayer, but rather just request tiles from 
+    // the server as normally done with leaflet. 
+    LTVis.addCanvasLayer(datasetID);
 
-    function row(d) {
-      var k,
-          dates = [];
 
-      for (k in d) {
-        if(d.hasOwnProperty(k)){
-          
-          dates.push()
-        }
-      }
+    // Style the tiles according to the default styling specified by a metadata
+    // file.
+
+    // initialize the timeline slider with new dates according to the metadata
+    // for this dataset.
+    // TODO: For now there is no metadata for the dataset. Indeed, there are no 
+    // proper datasets. So some metadata is invented here.
+    var dates = [];
+    for (var i = 1980; i <= 2020; i += 1) {
+      dates.push(i);
     }
 
-    d3.csv(url, row, function(data) {
-      var yes = data;
-      console.log(yes);
-    });
   }
 }); // END $.extend(LTVis, {...})
 
@@ -781,7 +822,7 @@ LTVis.GUI = (function() {
       // close the modal
       $(".modal").css("display", "none"); 
       // load the data associated with the id
-      LTVis.addCanvasLayer(id);
+      LTVis.loadDataset(id);
     });
   }
 
@@ -829,6 +870,8 @@ LTVis.GUI = (function() {
       initRasterDatasetSelections();
       initMenus();
       initSummaryPolygonSelections();
+
+      // initialize a timeline slider
 
       $(".chartMenuCancelButton").click(function() {
         cancelChartSetup();
@@ -879,6 +922,8 @@ LTVis.GUI = (function() {
         hideAllChartMenuPanels();
         $("#addSummaryPolygonOptions").show();
       });
+
+      // Load the starting dataset. 
     }
   };
 
@@ -890,46 +935,6 @@ var d3Map;
 // Start the app!
 $(document).ready(function() {
   LTVis.init();
-
-  // Everything from this point is experimental development code. 
-
-  // demo data for the timeline
-  var data = [
-    {date: 1970, value: 10},
-    {date: 1981, value: 20},
-    {date: 1982, value: 30},
-    {date: 1983, value: 10},
-    {date: 1984, value: -90},
-    {date: 1985, value: 80},
-    {date: 1986, value: 120},
-    {date: 1987, value: 2},
-    {date: 1988, value: 40},
-    {date: 1989, value: 50},
-    {date: 1991, value: 111},
-    {date: 1993, value: 22},
-    {date: 1994, value: 20},
-    {date: 1997, value: 33},
-    {date: 2000, value: 60},
-    {date: 2006, value: 99},
-    {date: 2011, value: 50},
-  ];
-
-  // Demo-ing the timeline here.
-  // TODO move this code into the GUI module when it's really time to add this.
-  var timechart = new LTVis.TimelineChart("timeRangeContainer", data);
-  timechart.on("change", function() {
-    console.log(timechart.getSelectedDataPoint());
-  });
-
-  LTVis.activeTimelineChart = timechart;
-
-  // var csvpath = "data/premadeAreaSummaries/fourStates_data.csv";
-  // d3.csv(csvpath, function(d){
-  //   console.log(d);
-
-  //   d3Map = d3.map(d);
-  //   console.log(d3Map);
-  // });
 });
 
 
