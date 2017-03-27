b6c0cd9c9017aeb0733997b62be3ca3d21b9efab
stephdan <stephdan@onid.oregonstate.edu>
2017-03-27 09:49:57 -0700

    Separated the chart code into separate file

---
+++ main.js
@@ -0,0 +1,6 @@
+// Launch the app and configure the default start-up settings.
+// TODO right now initial configuration happens in the module scripts. 
+// Might should be moved in here.
+$(document).ready(function() {
+  LTVis.init();
+});
